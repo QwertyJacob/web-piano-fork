8b8c5f8c1fae4db0084d8d70f58b2ec29e5e0b80
marcofilosa <marcofilosa22@gmail.com>
2022-10-21 16:50:34 +0200

    i've changed the noteOn function, adding the last two lines inside the noteOff function.

--- webaudioapis.js
+++ webaudioapis.js
@@ -7,6 +7,8 @@ var release = 0.1;
 var gains = [];
 var oscillators = [];
 
+var freqs = [523.25, 554.37,  587.33, 622.25,  659.25,  698.46,  739.99,  783.99,  830.61,  880.00,  932.33,  987.77]
+
 function noteOn(freq) {
     var o = c.createOscillator();
     var g = c.createGain();
@@ -18,16 +20,23 @@ function noteOn(freq) {
     o.frequency.setValueAtTime(freq, c.currentTime);
     g.gain.setValueAtTime(0, c.currentTime);
     g.gain.linearRampToValueAtTime(1, c.currentTime + attack);
+
+
+    g.gain.linearRampToValueAtTime(0.1, c.currentTime + release);
+    o.stop(c.currentTime + release);
 }
 
 function noteOff(freq) {
     var g = gains[freq]
     var o = oscillators[freq];
     g.gain.exponentialRampToValueAtTime(0.1, c.currentTime + release);
-    //o.stop(c.currentTime + release);
+    o.stop(c.currentTime + release);
 }
-keys = "awsedftgyhujk";
+
+const keys = "awsedftgyhujk";
+
 function noteToFreq(note) {return 440*Math.pow(2,note/12)}
 
-document.body.onkeydown = function (e) {if (!e.repeat) noteOn(noteToFreq(keys.indexOf(e.key)))}
-document.body.onkeyup = function(e) {noteOff(noteToFreq(keys.indexOf(e.key)))}
+
+//document.body.onkeydown = function (e) {if (!e.repeat) noteOn(noteToFreq(keys.indexOf(e.key)))}
+//document.body.onkeyup = function(e) {noteOff(noteToFreq(keys.indexOf(e.key)))}
