38b35a5f15dd097053f2f52651bfe578e2e2ca96
harryfoley <hfoley03@gmail.com>
2022-10-21 16:28:42 +0200

    Just checking

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const WHITE_KEYS = ["z", "x", "c", "v", "b", "n", "m", ","];
 const BLACK_KEYS = ["s", "d", "g", "h", "j"];
-
+//test: hello?
 const keys = document.querySelectorAll(".key");
 const whiteKeys = document.querySelectorAll(".key.white");
 const blackKeys = document.querySelectorAll(".key.black");
