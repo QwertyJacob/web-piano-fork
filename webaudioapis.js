de4a35c7266f5dacf2c532173de77a38ec4f458a
thepihen <96073767+thepihen@users.noreply.github.com>
2022-10-28 15:58:17 +0200

    added key functionality

--- webaudioapis.js
+++ webaudioapis.js
@@ -12,7 +12,7 @@ comp.connect(c.destination);
 
 
 
-function noteOn(freq) {
+function noteOnOffMouse(freq) {
     let o = c.createOscillator();
     let g = c.createGain();
     gains[freq] = g;
@@ -28,6 +28,19 @@ function noteOn(freq) {
     g.gain.linearRampToValueAtTime(0.1, c.currentTime + release);
     o.stop(c.currentTime + release);
 }
+function noteOn(freq) {
+    let o = c.createOscillator();
+    let g = c.createGain();
+    gains[freq] = g;
+    oscillators[freq] = o;
+    o.connect(g);
+    o.start();
+    g.connect(comp);
+    o.frequency.setValueAtTime(freq, c.currentTime);
+    g.gain.setValueAtTime(0, c.currentTime);
+    g.gain.linearRampToValueAtTime(1, c.currentTime + attack);
+}
+
 
 /*
 * This function is going to turn off the sound
@@ -42,14 +55,13 @@ function noteOff(freq) {
 function noteToFreq(note) {return 440*Math.pow(2,note/12)}
 
 
-/*
+
 document.body.onkeydown = function (e) {
     if (!e.repeat) {
-
-        let index_of_key = keys.indexOf(e.key)
-
-        let computed_frequency = noteToFreq(index_of_key)
-
+        let index_of_note = exercise_keys.indexOf(e.key)
+        //let index_of_key = keys.indexOf(e.key)
+        let computed_frequency = noteToFreq(index_of_note)
+        console.log(computed_frequency)
         noteOn(computed_frequency)
 
     }
@@ -59,8 +71,7 @@ document.body.onkeydown = function (e) {
 
 document.body.onkeyup = function(e) {
   let index_of_note = exercise_keys.indexOf(e.key)
-  let referenced_frequency =  hardcoded_frequencies[index_of_note]
-  noteOff(referenced_frequency)
+  //let referenced_frequency =  hardcoded_frequencies[index_of_note]
+  let computed_frequency = noteToFreq(index_of_note)
+  noteOff(computed_frequency)
 }
-
-*/
